8f7a02d17875f46d0f33da97b866c03ec7e29097
alex <alexu.werner@gmail.com>
2024-09-09 20:43:10 +0200

    Add offline events support

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import NProgress from 'nprogress';
 import mockData from './mock-data';
 
 //This function takes an events array, then uses map to create a new array with only locations.
@@ -24,6 +25,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -32,6 +39,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
