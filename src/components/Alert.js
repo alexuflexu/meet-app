856ea893c33b8b9836844612ea4f344c0a3ddf94
alex <alexu.werner@gmail.com>
2024-09-09 21:56:51 +0200

    update alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -51,7 +51,7 @@ class ErrorAlert extends Alert {
 class WarningAlert extends Alert { 
   constructor(props) {
     super(props);
-    this.color = "rgb(100, 100, 0)";
+    this.color = "rgb(150, 175, 55)";
     this.bgColor = "rgb(255, 255, 220)";
   }
 }
